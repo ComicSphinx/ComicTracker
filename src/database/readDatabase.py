1bf4c4af62bb2258abc3bfc7a42cc435fe70311e
Desasth <invicems@gmail.com>
2021-01-17 16:01:21 +0400

    variable was renamed

--- src/database/readDatabase.py
+++ src/database/readDatabase.py
@@ -5,8 +5,8 @@ def main():
     connection = sqlite3.connect('database.db')
     cursor = connection.cursor()
     cursor.execute("SELECT * FROM records")
-    results = cursor.fetchall()
-    print(results)
+    result = cursor.fetchall()
+    print(result)
 
 if __name__ == '__main__':
     main()

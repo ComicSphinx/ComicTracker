04be1b644c2e112b0749997f4a932edeb09b66c8
Desasth <invicems@gmail.com>
2021-01-25 23:54:07 +0400

    Plot.py updated.

--- src/Plot.py
+++ src/Plot.py
@@ -6,9 +6,16 @@ class Plot():
     def showPlot(self, year, month, day):
         """ Get data from database """
         data = dbu.getDataByYearMonthDay(dbu, year, month, day)
+        """ Prepare variables to plot """
         records,minutes = self.prepareVariablesToPlot(self, data)
-        plt.bar(records,minutes)
+        """ Create plot"""
+        plt.bar(records,minutes, 0.05)
+        self.configurePlot(self)
         plt.show()
+
+    def configurePlot(self):
+        plt.suptitle("Your doings")
+        plt.ylabel("Minutes")
 
     def prepareVariablesToPlot(self, data):
         x = []

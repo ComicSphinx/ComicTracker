ea5807bfdda38c140d5ea7a4ccb03295e96915a1
Desasth <invicems@gmail.com>
2021-02-14 00:47:56 +0400

    update PiePlot, added 'unknown' into the pie.

--- src/PiePlot.py
+++ src/PiePlot.py
@@ -8,18 +8,31 @@ from datetime import datetime as dt
 class PiePlot():
     
     str_sleeping = "Sleeping"
+    str_unknown = "?"
+    int_maxMinutes = 1440
+    int_sleep = 480
     
     def showPiePlot(self, records, minutes):
-        self.configurePiePlot(self)
+        self.configurePiePlot(self, minutes)
         plt.pie(minutes, labels=records, autopct='%1.0f%%')
         plt.show()
 
     def addSleep(self):
-        insert = dbu.buildInsert(dbu, self.str_sleeping, 480)
+        insert = dbu.buildInsert(dbu, self.str_sleeping, self.int_sleep)
         dbu.addDataToDB(dbu, insert)
 
-    def configurePiePlot(self):
+    def addUnknown(self, minutes):
+        tmp = 0
+        for i in range(len(minutes)):
+            tmp += minutes[i]
+        tmp = self.int_maxMinutes - tmp
+        insert = dbu.buildInsert(dbu, self.str_unknown, tmp)
+        dbu.addDataToDB(dbu, insert)
+
+    def configurePiePlot(self, minutes):
         mpl.rcParams['toolbar'] = 'none'
         if (dbu.dataIsNotExist(dbu, dt.now().year, dt.now().month, dt.now().day, self.str_sleeping)):
             self.addSleep(self)
+        if (dbu.dataIsNotExist(dbu, dt.now().year, dt.now().month, dt.now().day, self.str_unknown)):
+            self.addUnknown(self, minutes)
         
